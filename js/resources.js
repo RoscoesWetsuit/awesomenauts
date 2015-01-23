cf4ef88df51920a9c2fdf3cc9b671b183fc63359
Cade Guevara <cadeguevara@gmail.com>
2015-01-23 12:37:58 -0800

    finished video 4 and added comments

--- js/resources.js
+++ js/resources.js
@@ -1,9 +1,11 @@
 game.resources = [
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
+
+	 {name: "backgeound-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "meta-tiles", "type:"image", src: "data/img/meta-tiles.png}
+   
+    /*added the images to the code
+    */ 
 
 	/* Atlases 
 	 * @example
@@ -16,6 +18,9 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
 
+ 	 {name: "map01", type:"tmx", src: "data/map/map01.tmx"}
+ 	 /*loaded the map
+ 	 */
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
