3e8ba6ad6fb6af5544d2ad2347c77960cb425e55
Cade Guevara <cadeguevara@gmail.com>
2015-02-11 12:25:30 -0800

    finished video 20: Making Creeps Move

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -231,8 +231,17 @@ game.EnemyCreep = me.Entity.extend({
 		this.renderable.setCurrentAnimation("walk");
 	},
 
-	update: function(){
+	update: function(delta){
+		
 
+		this.body.vel.x -= this.accel.x * me.timer.tick
+
+		this.body.update(delta);
+
+
+		this._super(me.Entity, "update", [delta]);
+
+		return true
 	}
 });
 
