483c1fe5e27926abc1282cd537dcecf4c12f431a
cade guevara <cadeguevara@gmail.com>
2015-03-06 12:19:50 -0800

    finished video 39: Start New

--- js/screens/title.js
+++ js/screens/title.js
@@ -4,29 +4,37 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-		//shows title screen
-		me.input.bindKey(me.input.KEY.ENTER, "start");
 	
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function() {
-				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height])
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				// making a call to the super function
 				this.font = new me.Font("Arial", 46, "white");
 				// setting a font on the screen
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
+				),
+				//its waiting for the mouse to be clicked down		//when mouse is clicked we call that new game function 	
 			}, 
 
 			draw: function(renderer) {
-				this.font.draw(renderer.getContext(), "Awesomenauts", 450, 130);
-				//passing the context of where we are
-				this.font.draw(renderer.getContext(), "press ENTER to play!", 250, 530);
+				this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt){
+return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.save.remove('exp');
+				me.save.remove('exp2');
+				me.save.remove('exp3');
+				me.save.remove('exp4');
+				me.stat.change(me.state.PLAY);
 			}
 		})));
 
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
-			if(action=== "start") {
-				me.state.change(me.state.PLAY);
-			}
-		});
+
 
 	},
 	
@@ -35,7 +43,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-	me.input.unbindKey(me.input.KEY.ENTER);
-	me.event.unsubscribe(this.handler);
+
 	}
 });
