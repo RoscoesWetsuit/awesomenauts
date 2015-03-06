c5535c4880307c55db540f6f9cb1e92cdd4fe642
cade guevara <cadeguevara@gmail.com>
2015-03-06 12:23:35 -0800

    finished video 40: Continue

--- js/screens/title.js
+++ js/screens/title.js
@@ -34,6 +34,31 @@ return true;
 			}
 		})));
 
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				// making a call to the super function
+				this.font = new me.Font("Arial", 46, "white");
+				// setting a font on the screen
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
+				),
+				//its waiting for the mouse to be clicked down		//when mouse is clicked we call that new game function 	
+			}, 
+
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt){
+				return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.stat.change(me.state.PLAY);
+			}
+		})));		
+
 
 
 	},
