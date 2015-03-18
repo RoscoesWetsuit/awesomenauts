ca8cc2ebbb3f52badbe4d6e762cf417f64f12532
cade guevara <cadeguevara@gmail.com>
2015-03-17 23:07:47 -0700

    finished video 47: Drawing spendGold image

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -97,10 +97,40 @@ game.SpendGold = Object,extend({
 		this.lastBuy = new Date()
 		this.paused = false;.getTime();
 		this.alwaysUpdate = true;
+		this.updateWhenPaused = true;
+		this.buying = false;
 	},
 
 	update: function(){
+		if(me.input.isKeyPressed("buy") && this.now-this.lastbuy >=1000){
+			this.lastBuy = this.now;
+			if(!this.buying){
+				this.startBuying();
+			}else{
+				this.stopBuying();
+			}
+		}
 
+		return true;
+	},
+
+	startBuying: function(){
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+		game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyscreen.updateWhenPaused = true;
+		game.data.buyscreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0,0);
+	},
+
+	stopBuying: function(){
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen)
 	}
+
 
 })
