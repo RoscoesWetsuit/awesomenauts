fbb657e5ef8e33fd59f18d80b477c5e257f25d44
cade guevara <cadeguevara@gmail.com>
2015-03-17 22:30:21 -0700

    finished video 45: Increasing Gold Production Based on game data exp1

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -10,6 +10,7 @@ game.SpendExp = me.ScreenObject.extend({
 		me.input.bindKey(me.input.KEY.F3, "F3");
 		me.input.bindKey(me.input.KEY.F4, "F4");
 		me.input.bindKey(me.input.KEY.F5, "F5");
+		var exp1 cost = ((game.data.exp1) + 10);
 
 
 		me.game.world.addChild(new (me.Renderable.extend({
@@ -24,7 +25,7 @@ game.SpendExp = me.ScreenObject.extend({
 			draw: function(renderer) {
 				this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString, this.pos.x + 150, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION CURRENT LEVEL: " + game.data.exp1.toString() + " COST: " + ((game.data.exp1) + 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION CURRENT LEVEL: " + game.data.exp1.toString() + " COST: " + exp1cost, this.pos.x, this.pos.y + 100);
 				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD ", this.pos.x, this.pos.y + 150);
 				this.font.draw(renderer.getContext(), "F3: INCREASE DAMAGE ", this.pos.x, this.pos.y + 200);											
 				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH", this.pos.x, this.pos.y + 250);
@@ -34,7 +35,13 @@ game.SpendExp = me.ScreenObject.extend({
 
 		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCodee, edge){
 			if(action === "F1"){
-
+				if(game.data.exp1 >= exp1cost){
+					game.data.exp1 += 1;
+					game.data.exp -= exp1cost;
+					me.state.change(me.state.PLAY);
+				}else{
+					console.log("not enouugh experience");
+				}
 			}else if(action === "F2"){
 
 			}else if(action === "F3"){
