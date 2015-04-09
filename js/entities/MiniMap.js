47bbb3bd59bf5d97c0034642266ad381ede6707d
cade guevara <cadeguevara@gmail.com>
2015-04-08 22:56:49 -0700

    finished video i6: Displaying your Minimap Intermediate

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,15 @@
 game.MiniMap = me.Entity.extend ({
-	
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y {
+			image: :minimap
+			width: 282,
+			height: 158,
+			spritewidth: "282",
+			spriteheight: "158",
+			getShape: function(){
+				return (new me.Rect(0, 0, 282. 158)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+	}
 });
